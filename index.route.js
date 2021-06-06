299e5a5792d99b6ff1770c129bcc50adffc01efe
samrachuni <samrachuni@gmail.com>
2021-06-06 16:17:02 +0200

    add product to route

--- index.route.js
+++ index.route.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const userRoutes = require('./server/user/user.route');
 const authRoutes = require('./server/auth/auth.route');
+const productRoute = require('./server/product/product.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 // TODO: use glob to match *.route files
 
 /** GET /health-check - Check service health */
-router.get('/health-check', (req, res) =>
-  res.send('OK')
-);
+router.get('/health-check', (req, res) => res.send('OK'));
 
 // mount user routes at /users
 router.use('/users', userRoutes);
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
+
+// mount product routes at /product
+
+router.use('/product', productRoute);
 
 module.exports = router;
